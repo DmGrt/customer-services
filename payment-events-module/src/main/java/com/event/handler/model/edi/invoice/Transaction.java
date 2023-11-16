9d647e9c19d0c85a9abb77b5df627599626af64e
DmGrt <grytsishin@gmail.com>
2023-11-16 12:27:23 +0200

    payment events import

--- payment-events-module/src/main/java/com/event/handler/model/edi/invoice/Transaction.java
+++ payment-events-module/src/main/java/com/event/handler/model/edi/invoice/Transaction.java
@@ -3,6 +3,7 @@ package com.event.handler.model.edi.invoice;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 import java.util.ArrayList;
 @Data
